e672fd1c156110a0ae7f12e71e49c76f76a57db8
sergeimuravsky <murawski.radiant@gmail.com>
2021-11-07 23:39:58 +0300

    fix: add exports for all components

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -3,9 +3,11 @@ export * from '@seed-ui/layout';
 export * from '@seed-ui/styles';
 
 export * from './components/Box';
-export * from './components/Title';
 export * from './components/Button';
 export * from './components/Caption';
+export * from './components/IconButton';
+export * from './components/InputGroup';
+export * from './components/Title';
 export * from './components/Label';
 export * from './components/OList';
 export * from './components/ListItem';
@@ -15,6 +17,7 @@ export * from './components/Subtitle';
 export * from './components/Text';
 export * from './components/UList';
 export * from './components/Link';
-export * from './components/IconButton';
+export * from './components/TextInput';
+export * from './components/Textarea';
 
 export * from './styles/helpers';
