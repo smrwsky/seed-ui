865e4814cc5e2fa940b9555775afa193ad678311
Sergey Murawsky <smrwsky@gmail.com>
2022-10-15 19:14:33 +0400

    feature(layout): Add new breakpoints to Hidden component

--- packages/layout/src/components/Hidden/Hidden.tsx
+++ packages/layout/src/components/Hidden/Hidden.tsx
@@ -5,30 +5,57 @@ import * as S from './Hidden.css';
 
 export type HiddenProps = {
   children?: React.ReactNode;
+  desktop?: boolean;
+  desktopDown?: boolean;
   desktopUp?: boolean;
+  desktopLg?: boolean;
+  desktopLgDown?: boolean;
+  desktopLgUp?: boolean;
+  desktopXlUp?: boolean;
   mobileDown?: boolean;
+  mobileLg?: boolean;
+  mobileLgDown?: boolean;
+  mobileLgUp?: boolean;
   tablet?: boolean;
   tabletDown?: boolean;
   tabletUp?: boolean;
 };
 
 function Hidden({
+  desktop,
+  desktopDown,
+  desktopUp,
+  desktopLg,
+  desktopLgDown,
+  desktopLgUp,
+  desktopXlUp,
   mobileDown,
+  mobileLg,
+  mobileLgDown,
+  mobileLgUp,
   tablet,
   tabletDown,
   tabletUp,
-  desktopUp,
   children,
 }: HiddenProps): JSX.Element {
   return (
     <div
       className={cn(
         S.root,
+        desktop && S.desktop,
+        desktopDown && S.desktopDown,
+        desktopUp && S.desktopUp,
+        desktopLg && S.desktopLg,
+        desktopLgDown && S.desktopLgDown,
+        desktopLgUp && S.desktopLgUp,
+        desktopXlUp && S.desktopXlUp,
         mobileDown && S.mobileDown,
+        mobileLg && S.mobileLg,
+        mobileLgDown && S.mobileLgDown,
+        mobileLgUp && S.mobileLgUp,
         tablet && S.tablet,
         tabletDown && S.tabletDown,
         tabletUp && S.tabletUp,
-        desktopUp && S.desktopUp,
       )}
     >
       {children}
