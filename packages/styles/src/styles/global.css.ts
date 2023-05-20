82c1f8480f2454cc1b31bea7f244efee96b3e7c9
Sergey Murawsky <smrwsky@gmail.com>
2023-05-20 13:44:59 +0400

    fix: Fix ThemeProvider

--- packages/styles/src/styles/global.css.ts
+++ packages/styles/src/styles/global.css.ts
@@ -1,8 +1,14 @@
-import { createGlobalTheme, globalStyle } from '@vanilla-extract/css';
+import {
+  createTheme,
+  createThemeContract,
+  globalStyle,
+} from '@vanilla-extract/css';
 
 import { DEFAULT_THEME } from '../const';
 
-export const vars = createGlobalTheme(':root', DEFAULT_THEME);
+export const vars = createThemeContract(DEFAULT_THEME);
+
+export const defaultTheme = createTheme(vars, DEFAULT_THEME);
 
 globalStyle('body', {
   color: vars.color.neutral900,
