c2d721ac33c81c28d65df30818e78c14484155e8
Sergey Murawsky <smrwsky@gmail.com>
2023-10-17 19:23:35 +0400

    feat: Update input global styles

---
+++ .changeset/strong-apes-act.md
@@ -0,0 +1,5 @@
+---
+'@seed-ui/styles': minor
+---
+
+Updated input global styles.
--- packages/styles/src/styles/global.css.ts
+++ packages/styles/src/styles/global.css.ts
@@ -10,130 +10,137 @@ export const vars = createThemeContract(DEFAULT_THEME);
 
 export const defaultTheme = createTheme(vars, DEFAULT_THEME);
 
-globalStyle('*', {
-  WebkitTapHighlightColor: 'transparent',
-});
+// Ignore the following lines in development mode because they are conflicting
+// with bundled styles.
 
-globalStyle('body', {
-  color: vars.color.neutral900,
-  fontFamily: vars.fontFamily.primary,
-  fontSize: vars.fontSize.md,
-  fontWeight: vars.fontWeight.regular,
-  letterSpacing: vars.letterSpacing.wider,
-  lineHeight: vars.lineHeight.normal,
-  fontSmooth: 'antialiased',
-  textRendering: 'optimizeLegibility',
-});
+if (process.env.NODE_ENV !== 'development') {
+  globalStyle('*', {
+    WebkitTapHighlightColor: 'transparent',
+  });
 
-globalStyle('ol, ul', {
-  paddingInlineStart: '1.5em',
-});
-
-globalStyle('ul', {
-  listStyle: 'none',
-});
-
-globalStyle('li', {
-  position: 'relative',
-});
-
-globalStyle('li+li', {
-  marginTop: '.5em',
-});
-
-globalStyle('a', {
-  color: vars.color.primary500,
-  cursor: 'pointer',
-  textDecoration: 'none',
-  transition: vars.transition.base,
-});
-
-globalStyle('a:hover', {
-  textDecoration: 'underline',
-});
-
-globalStyle('a:visited', {
-  color: vars.color.secondary500,
-});
-
-globalStyle('img', {
-  fontFamily: vars.fontFamily.primary,
-  fontSize: 'xs',
-  letterSpacing: 'wide',
-  lineHeight: 'snug',
-});
-
-globalStyle('button, [type="button"], [type="submit"]', {
-  border: 'none',
-  color: 'inherit',
-  fontFamily: 'inherit',
-  fontSize: 'inherit',
-  fontWeight: 'inherit',
-  letterSpacing: 'inherit',
-  lineHeight: 'inherit',
-  background: 'transparent',
-  WebkitTapHighlightColor: 'transparent',
-  padding: 0,
-  cursor: 'pointer',
-});
-
-globalStyle(
-  'button:disabled, [type="button"]:disabled, [type="submit"]:disabled',
-  {
-    cursor: 'not-allowed',
-  },
-);
-
-globalStyle('input, select, textarea', {
-  outline: 'none',
-});
-
-globalStyle(
-  'input:not([type="checkbox"]):not([type="radio"]), select, textarea',
-  {
-    flex: 1,
-    maxWidth: '100%',
-    width: '100%',
-    borderRadius: 0,
-    border: 0,
+  globalStyle('body', {
+    color: vars.color.neutral900,
     fontFamily: vars.fontFamily.primary,
     fontSize: vars.fontSize.md,
     fontWeight: vars.fontWeight.regular,
     letterSpacing: vars.letterSpacing.wider,
     lineHeight: vars.lineHeight.normal,
+    fontSmooth: 'antialiased',
+    textRendering: 'optimizeLegibility',
+  });
+
+  globalStyle('ol, ul', {
+    paddingInlineStart: '1.5em',
+  });
+
+  globalStyle('ul', {
+    listStyle: 'none',
+  });
+
+  globalStyle('li', {
+    position: 'relative',
+  });
+
+  globalStyle('li+li', {
+    marginTop: '.5em',
+  });
+
+  globalStyle('a', {
+    color: vars.color.primary500,
+    cursor: 'pointer',
+    textDecoration: 'none',
+    transition: vars.transition.base,
+  });
+
+  globalStyle('a:hover', {
+    textDecoration: 'underline',
+  });
+
+  globalStyle('a:visited', {
+    color: vars.color.secondary500,
+  });
+
+  globalStyle('img', {
+    fontFamily: vars.fontFamily.primary,
+    fontSize: 'xs',
+    letterSpacing: 'wide',
+    lineHeight: 'snug',
+  });
+
+  globalStyle('button, [type="button"], [type="submit"]', {
+    border: 'none',
+    color: 'inherit',
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    fontWeight: 'inherit',
+    letterSpacing: 'inherit',
+    lineHeight: 'inherit',
     background: 'transparent',
-    padding: '0 0.25rem',
-  },
-);
+    WebkitTapHighlightColor: 'transparent',
+    padding: 0,
+    cursor: 'pointer',
+  });
 
-globalStyle(
-  'input:not([type="checkbox"]):not([type="radio"]):not([type="range"]):disabled, select:disabled, textarea:disabled',
-  {
+  globalStyle(
+    'button:disabled, [type="button"]:disabled, [type="submit"]:disabled',
+    {
+      cursor: 'not-allowed',
+    },
+  );
+
+  globalStyle('input, select, textarea', {
+    outline: 'none',
+  });
+
+  globalStyle(
+    'input:not([type="checkbox"]):not([type="radio"]), textarea, select',
+    {
+      flex: 1,
+      maxWidth: '100%',
+      width: '100%',
+      borderRadius: 0,
+      border: 0,
+      fontFamily: vars.fontFamily.primary,
+      fontSize: vars.fontSize.md,
+      fontWeight: vars.fontWeight.regular,
+      letterSpacing: vars.letterSpacing.wider,
+      lineHeight: vars.lineHeight.normal,
+      background: 'transparent',
+      padding: '0 0.25rem',
+    },
+  );
+
+  globalStyle(
+    'input:not([type="checkbox"]):not([type="radio"]):not(:placeholder-shown):disabled,' +
+      'textarea:not(:placeholder-shown):disabled,' +
+      'select:disabled',
+    {
+      color: vars.color.neutral300,
+      cursor: 'not-allowed',
+      opacity: 1,
+      WebkitTextFillColor: vars.color.neutral300,
+    },
+  );
+
+  globalStyle('select', {
+    appearance: 'none',
+  });
+
+  globalStyle('label', {
+    display: 'block',
+  });
+
+  globalStyle('::placeholder', {
     color: vars.color.neutral200,
-    cursor: 'not-allowed',
     opacity: 1,
-    WebkitTextFillColor: vars.color.neutral200,
-  },
-);
+  });
 
-globalStyle('select', {
-  appearance: 'none',
-});
+  globalStyle('::-ms-input-placeholder', {
+    color: vars.color.neutral200,
+  });
 
-globalStyle('textarea', {
-  resize: 'none',
-});
-
-globalStyle('::placeholder', {
-  color: vars.color.neutral100,
-  opacity: 1,
-});
-
-globalStyle(':focus-visible:not(input):not(select):not(textarea)', {
-  outline: 'none',
-  boxShadow: vars.boxShadow.focus,
-});
-
-globalStyle('label', {
-  display: 'block',
-});
+  globalStyle(':focus-visible:not(input):not(select):not(textarea)', {
+    outline: 'none',
+    boxShadow: vars.boxShadow.focus,
+  });
+}
